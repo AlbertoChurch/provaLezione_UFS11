67c62a712f0cddffcb6024fd12e8a815dccb3ba6
Alberto Chiesa <158302513+AlbertoChurch@users.noreply.github.com>
2025-11-21 17:49:46 +0100

    Delete template-progetto/src/main/java/com/mycompany/app/Motore.java

--- template-progetto/src/main/java/com/mycompany/app/Motore.java
+++
@@ -1,5 +0,0 @@
-public class Motore extends Veicolo{
-    public Motore(String marca, String modello, String tipo, String alimentzione, int annoImmatricolazione, int prezzo, int cilindrata) {
-        super(marca, modello, tipo, alimentzione, annoImmatricolazione, prezzo,  cilindrata);
-    }
-}
