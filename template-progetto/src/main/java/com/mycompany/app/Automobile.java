adf393f56fb69f04cde202415714af36f1ebce2a
Alberto Chiesa <158302513+AlbertoChurch@users.noreply.github.com>
2025-11-21 17:50:09 +0100

    Delete template-progetto/src/main/java/com/mycompany/app/Automobile.java

--- template-progetto/src/main/java/com/mycompany/app/Automobile.java
+++
@@ -1,5 +0,0 @@
-public  class Automobile extends Veicolo {
-    public Automobile(String marca, String modello, String tipo, String alimentzione, int annoImmatricolazione, int prezzo, int cilindrata) {
-        super(marca, modello, tipo, alimentzione, annoImmatricolazione, prezzo,cilindrata);
-    }
-}
